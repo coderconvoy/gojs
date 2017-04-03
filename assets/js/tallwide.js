cea12b5d7e04714e47c1b220e3022929dc776a16
coderconvoy <thebeaky@psmail.net>
2017-04-03 15:28:19 +0100

    Added Tall or wide funcs

---
+++ assets/js/tallwide.js
@@ -0,0 +1,32 @@
+
+
+function tallOrWide(frac){
+    if ! frac {
+        frac = 1.3
+    }
+    
+    torwide = function(){
+        var mDiv = document.getElementById("main-area");
+        if (window.innerWidth > window.innerHeight * frac){
+            console.log("Wide");
+            mDiv.className = "main-wide";
+        }else {
+            console.log("Tall");
+            mDiv.className = "main-tall";
+        }
+    }
+    
+    window.addEventListener('resize',torwide);
+
+    torwide()
+    return function(n){
+        if n === undefined {
+            return n
+        }
+        frac = n
+    }
+}
+
+
+TallFrac = tallOrWide();
+ 
